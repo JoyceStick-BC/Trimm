d78705e28f532b7981f0e34d4ce94a56a17ae7bf
Austin Bailey <baileyau@bc.edu>
2018-02-10 17:34:42 -0500

    Added Organization model

---
+++ app/Models/Organization.php
@@ -0,0 +1,17 @@
+<?php
+namespace Carbon\Models;
+
+//extend eloquent to connect to DB
+use Illuminate\Database\Eloquent\Model;
+
+
+class Organization extends Model {
+    //if the table isnt the plural of the class name, use this:
+    protected $table = 'organizations';
+
+    //define which columns can be written to
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+}
